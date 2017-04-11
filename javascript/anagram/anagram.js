81e8f09b66d9b43dedb108c3005d48f01bd83884
SFoskitt <stephanie.foskitt@gmail.com>
2017-04-10 21:39:59 -0700

    needs much more work, but better than yesterday 'anagram'

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,10 +1,26 @@
-Anagram = function (arg){
-
+Anagram = function(arg){
+    this.word = arg.toLowerCase().split('').sort();
 }
 
 Anagram.prototype.matches = function (arr) {
-	
+    // console.log('this.word in matches', this.word);
+    var process = this.process;
+    var base = this.word;
+    var ans = arr.filter(function(ele){
+        console.log('ele', ele);
+        return process(ele.toLowerCase().split('').sort(), base);
+    });
+    console.log('ans', ans);
+    return ans;
+}
 
+Anagram.prototype.process = function (test, base) {
+    console.log('JSON.stringify(test) === JSON.stringify(base)', JSON.stringify(test) === JSON.stringify(base));
+    return JSON.stringify(test) === JSON.stringify(base);
+}
+
+Anagram.prototype.sortA = function(a,b){
+    return a - b;
 }
 
 module.exports = Anagram;
