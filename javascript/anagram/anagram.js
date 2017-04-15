768d6b9b101e1f0bc0df565c0b6105889e06d7c6
SFoskitt <stephanie.foskitt@gmail.com>
2017-04-15 14:52:14 -0700

    revise anagram in class style

--- javascript/anagram/anagram.js
+++ javascript/anagram/anagram.js
@@ -1,16 +1,18 @@
-Anagram = function(arg){
-    this.arg = arg.toLowerCase();
-    this.word = arg.toLowerCase().split('').sort().join('');
-}
+class Anagram {
+    constructor(arg){
+        this.arg = arg.toLowerCase();
+        this.word = arg.toLowerCase().split('').sort().join('');
+    }
 
-Anagram.prototype.matches = function(thing){
-    let arr = Array.isArray(thing) ? thing : [thing];
-    return arr.filter((ele) => {
-        let toTest = ele.toLowerCase();
-        if(toTest !== this.arg){
-            return this.word === toTest.split('').sort().join('');        
-        }
-    });
+    matches(thing){
+        let arr = (Array.isArray(thing) ? thing : Array.apply(null, arguments));
+        return arr.filter((ele) => {
+            let toTest = ele.toLowerCase();
+            if(toTest !== this.arg){
+                return this.word === toTest.split('').sort().join('');
+            }
+        }, this)
+    }
 }
 
 module.exports = Anagram;
