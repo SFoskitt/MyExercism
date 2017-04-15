9aa3d157b23797de887686784fe3f41f05f6d873
SFoskitt <stephanie.foskitt@gmail.com>
2017-04-15 15:02:53 -0700

    rewrite isogram as class

--- javascript/isogram/isogram.js
+++ javascript/isogram/isogram.js
@@ -1,18 +1,18 @@
-Isogram = function (word) {
-    this.word = word;
-}
-
-Isogram.prototype.isIsogram = function(){
-    var dict = {};
-    var count = 0;
-
-    for (var i = 0; i < this.word.length; i++){
-        if(this.word[i].match(/[A-Z|a-z|ü|é]/i)){
-            dict[this.word[i].toLowerCase()] = this.word[i].toLowerCase();
-            count++;
-        }
+class Isogram {
+    constructor(word){
+        this.word = word.toLowerCase().split('');
     }
-    return count === Object.keys(dict).length;
-}
 
+    isIsogram(){
+        let dict = {};
+        let count = 0;
+        this.word.forEach((ele) => {
+            if(ele.match(/[A-Z|a-z|ü|é]/i)){
+                dict[ele] = ele;
+                count++;
+            }
+        }, this);
+        return count === Object.keys(dict).length;
+    }
+}
 module.exports = Isogram;
