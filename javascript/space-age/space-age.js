d1190d923ec491e638c4a72059a5dc9478d25550
SFoskitt <stephanie.foskitt@gmail.com>
2017-05-11 11:10:25 -0700

    finish space-age.js

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -20,31 +20,37 @@ class SpaceAge {
 
     onVenus () {
         // - Venus: orbital period 0.61519726 Earth years
-        return this.formNum(this.onEarth() * 0.61519726);
+        return this.formNum(this.onEarth() / 0.6155);
     }
 
     onMercury () {
         // - Mercury: orbital period 0.2408467 Earth years
+        return this.formNum(this.onEarth() / 0.2408467);
     }
 
     onJupiter () {
         // - Jupiter: orbital period 11.862615 Earth years
+        return this.formNum(this.onEarth() / 11.862615);
     }
 
     onSaturn () {
         // - Saturn: orbital period 29.447498 Earth years
+        return this.formNum(this.onEarth() / 29.447498);
     }
 
     onNeptune () {
         // - Neptune: orbital period 164.79132 Earth years
+        return this.formNum(this.onEarth() / 164.79132);
     }
 
     onUranus () {
         // - Uranus: orbital period 84.016846 Earth years
+        return this.formNum(this.onEarth() / 84.016846);
     }
 
     onMars () {
         // - Mars: orbital period 1.8808158 Earth years
+        return this.formNum(this.onEarth() / 1.8808158);
     }
 }
 
