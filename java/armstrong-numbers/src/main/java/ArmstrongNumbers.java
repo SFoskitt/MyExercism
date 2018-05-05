f2cf4f72021abaa9e3a6853f9c3e74bf53fccc9b
SFoskitt <stephanie.foskitt@gmail.com>
2018-05-05 11:22:16 -0500

    armstrongNumbers problem done

--- java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ java/armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,17 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
+		String numToString = Integer.toString(numberToCheck);
+		int len = numToString.length();
+		char[] processArr = numToString.toCharArray();
+		int accum = 0;
 
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		for(char digit : processArr){
+			int tmp = Character.getNumericValue(digit);
+			accum += Math.pow(tmp, len);
+		}
 		
+		return accum == numberToCheck;
 	}
 
 }
