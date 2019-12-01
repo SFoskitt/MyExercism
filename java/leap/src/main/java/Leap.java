64a1b6c98d7a22421126806890aa6002050851ad
SFoskitt <stephanie.foskitt@gmail.com>
2019-12-01 16:58:31 -0600

    java leap, and a bunch of other junk

---
+++ java/leap/src/main/java/Leap.java
@@ -0,0 +1,12 @@
+class Leap {
+
+    boolean isLeapYear(int year) {
+        if (year % 4 == 0 && !(year % 100 == 0)) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,12 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        String processStr = input.toLowerCase();
+        char[] processArr = input.toCharArray();
+
+
+        return false;
+    }
+
+}
