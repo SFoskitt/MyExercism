97da30f2591599ce399dcc605f9b280979015610
SFoskitt <SFoskitt@users.noreply.github.com>
2019-12-02 17:15:44 -0600

    darts!

--- java/darts/src/main/java/Darts.java
+++ java/darts/src/main/java/Darts.java
@@ -9,8 +9,9 @@ class Darts {
 
     int score() {
         double rad = Math.sqrt((calcX * calcX) + (calcY * calcY));
-        if (rad <= 10 && rad < 5) { return 1; }
-        if (rad <= 5 && rad < 1) { return 5; }
+        System.out.println("rad " + rad);
+        if (rad <= 10 && rad > 5) { return 1; }
+        if (rad <= 5 && rad > 1) { return 5; }
         if (rad <= 1) { return 10; }
         return 0;
     }
