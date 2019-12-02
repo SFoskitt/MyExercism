43bf33f5f70b9a6be3cd08a589766d99edd0f54d
SFoskitt <stephanie.foskitt@gmail.com>
2019-12-02 12:21:09 -0600

    darts - does not test

---
+++ java/darts/src/main/java/Darts.java
@@ -0,0 +1,18 @@
+class Darts {
+    private double calcX;
+    private double calcY;
+
+    Darts(double x, double y) {
+        calcX = x;
+        calcY = y;
+    }
+
+    int score() {
+        double rad = Math.sqrt((calcX * calcX) + (calcY * calcY));
+        if (rad <= 10 && rad < 5) { return 1; }
+        if (rad <= 5 && rad < 1) { return 5; }
+        if (rad <= 1) { return 10; }
+        return 0;
+    }
+
+}
